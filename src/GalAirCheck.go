a74b5168b79b7f13ec3c43e107d11d5db6d7ed82
Julien Senon <julien.senon@gmail.com>
2017-05-30 16:29:03 +0200

    main

---
+++ src/GalAirCheck.go
@@ -0,0 +1,25 @@
+//go:generate swagger generate spec
+// Package main GalAirCheck.
+//
+// the purpose of this application is to provide an Galera Check application
+// that will show state of Galera Cluster in real Time
+//
+// Terms Of Service:
+//
+// there are no TOS at this moment, use at your own risk we take no responsibility
+//
+//     Schemes:
+//     Host:
+//     BasePath:
+//     Version: 0.0.1
+//     License: MIT http://opensource.org/licenses/MIT
+//     Contact: Julien SENON <julien.senon@gmail.com>
+package main
+
+import ()
+
+// TO FIX
+
+func main() {
+
+}
